fd4a452296e16afdde6d9d4924b0070a6259e7c6
oskoss <pblum@pivotal.io>
2019-01-30 10:35:52 -0600

    final fix

--- tasks/ops-man/get-root-ca/task.sh
+++ tasks/ops-man/get-root-ca/task.sh
@@ -10,7 +10,8 @@ cert=`om-linux \
   --username "$OPS_MGR_USR" \
   --password "$OPS_MGR_PWD" \
   --format json \
-  certificate-authorities | jq -r ".[0].cert_pem"` 
-  
-echo $cert > ca-cert/root_ca.cert
+  certificate-authorities | jq ".[0].cert_pem"`
 
+echo -ne $cert > ca-cert/temp_root_ca.cert
+sed -e "s/\"//" ca-cert/temp_root_ca.cert  > ca-cert/root_ca.cert
+rm -f ca-cert/temp_root_ca.cert
