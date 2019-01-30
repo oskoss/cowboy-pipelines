829b0d6e8028f08bc1a98a074b680b7661dc185e
oskoss <pblum@pivotal.io>
2019-01-30 07:44:20 -0600

    attempt 2

--- tasks/ops-man/get-root-ca/task.sh
+++ tasks/ops-man/get-root-ca/task.sh
@@ -4,11 +4,13 @@ set -eo pipefail
 
 printf "Getting Root CA from $OPSMAN_DOMAIN_OR_IP_ADDRESS"
 
-echo -ne `om-linux \
+cert=`om-linux \
   --target https://$OPSMAN_DOMAIN_OR_IP_ADDRESS \
   --skip-ssl-validation \
   --username "$OPS_MGR_USR" \
   --password "$OPS_MGR_PWD" \
   --format json \
-  certificate-authorities | jq ".[0].cert_pem"` > ca-cert/root_ca.cert
+  certificate-authorities | jq -r ".[0].cert_pem"` 
+
+echo $cert > ca-cert/root_ca.cert
 
