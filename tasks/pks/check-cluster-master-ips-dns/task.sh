5b07387d0b5a3b7f299ff77eb6babdff4b71c1b1
oskoss <pblum@pivotal.io>
2019-02-02 09:17:09 -0600

    add check cluster dns step

---
+++ tasks/pks/check-cluster-master-ips-dns/task.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+
+set -eo pipefail
+
+cluster_fqdn=$(pks cluster $CLUSTER_NAME --json | jq -r '.parameters.kubernetes_master_host')
+cluster_ips_string=$(pks cluster $CLUSTER_NAME --json | jq -r '.kubernetes_master_ips[]')
+cluster_ips=($cluster_ips_string)
+
+printf "\nChecking if PKS $CLUSTER_NAME Cluster Master IPs resolve with DNS Server: $DNS_SERVER_IP......"
+
+set +eo pipefail
+
+printf "\n\nPLEASE ENSURE $cluster_fqdn RESOLVES TO ONE OR ALL OF THE FOLLOWING PKS CLUSTER MASTER IPS:"
+
+for elem in "${cluster_ips[@]}"
+do 
+printf "\n    $elem"
+done
+
+printf "\nCHECKING......"
+host "$cluster_fqdn" "$DNS_SERVER_IP"
+while [ $? -ne 0 ]; do
+  printf "FAILED"
+  sleep 10
+  printf "\n\nPLEASE ENSURE $cluster_fqdn RESOLVES TO ONE OR ALL OF THE FOLLOWING PKS CLUSTER MASTER IPS:"
+
+  for elem in "${cluster_ips[@]}"
+  do 
+    printf "\n    $elem"
+  done
+  host "$cluster_fqdn" "$DNS_SERVER_IP"
+done
+
+resolvedIp=$(host "$cluster_fqdn" "$DNS_SERVER_IP" | sed -n -e 's/^.*has address //p')
+for ip in "${cluster_ips[@]}"
+do
+    if [ "$ip" == "$resolvedIp" ] ; then
+        printf "Success :)"
+        exit 0
+    fi
+done
+
+printf "FAILED"
+printf "\nDNS Server $DNS_SERVER_IP resolved $cluster_fqdn to $resolvedIp but this is not ${cluster_ips[@]} Bailing out...."
+exit 1
